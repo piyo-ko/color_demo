40b31095a7fa03acb761c7c3575e6901f8fdb426
Piyo <piyo-ko@zj8.so-net.ne.jp>
2016-09-27 00:32:40 +0900

    :beetle: 256色のドット絵用のスクリプトを修正（正規表現の微調整)

--- pixel_art_256colors.pl
+++ pixel_art_256colors.pl
@@ -7,7 +7,7 @@ while (<>) {
   chomp;
   my @colors = split(/\s+/, $_);
   for (my $i=0; $i<=$#colors; $i++) {
-    if ($colors[$i] =~ /(^[12]?[0-9])?[0-9]$/) {
+    if ($colors[$i] =~ /^([12]?[0-9])?[0-9]$/) {
       if (0 <= $colors[$i] && $colors[$i] <= 255) {
         print "\e[48;5;$colors[$i]m  ";
       } else {
